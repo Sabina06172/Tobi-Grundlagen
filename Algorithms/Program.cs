c1da2e3977550abd8ed048acbfb9bec94b00714e
Sabina06172 <204861367+Sabina06172@users.noreply.github.com>
2025-04-08 13:13:13 +0200

    implemented BinarySearch

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,23 +1,48 @@
 ﻿int[] cards = [3, 5, 7, 8, 9];
-int[] cards2 = new int[5000000];
+
+
+int[] cards2 = new int[50000000];
 for (int i = 0; i < cards2.Length; i++)
 {
     cards2[i] = i +1;
 }
 
-
 int query = 7;
 
 var result = FindCardLocation(cards, query);
-System.Console.WriteLine(value: result);
+System.Console.WriteLine(value:"Index vom gesuchtem Element in cards:" + result);
 
 System.Console.WriteLine(cards2[10]);
-var result2 = FindCardLocation(cards2, query: 4_942_069);
-System.Console.WriteLine(result2);  //4_942_068 wird erwartet/erraten
+var result2 = FindCardLocationBinarySearch(cards2, query: 40_942_069);
+System.Console.WriteLine("Index vom gesuchtem Element in cards2:" + result2);  //40_942_068 wird erwartet/erraten
 
 
 //Binary Search: => O(log(n))
-// int FindCardLocationBinarySearch(int[] cards, int query)
+int FindCardLocationBinarySearch(int[] cards, int query)
+{
+    int low = 0;
+    int high = cards2.Length -1;
+    while(low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if(cards2[mid] == query)
+        {
+            return mid;
+        }
+        else if(cards2[mid] < query)
+        {
+            low = mid +1;
+        }
+        else
+        {
+            high = -1;
+        }
+        
+    }
+    return -1;
+    
+}
 
 
 
